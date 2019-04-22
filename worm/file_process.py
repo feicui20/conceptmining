557d833f2c457c93d5dc9a07c22a1d4f3810efb5
chenjn14 <chenjianan18@ict.ac.cn>
2019-04-22 21:12:43 +0800

    1978to2019sannong

---
+++ worm/file_process.py
@@ -0,0 +1,47 @@
+#coding:utf-8
+
+import glob
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+pd.set_option('display.max_columns', None)
+df = pd.DataFrame()
+pattern_author = re.compile(u"^作者：(.+)&")
+pattern_date = re.compile(u'时间：(\d+-\d+-\d+)')
+for name in glob.glob('C:\\Users\\chenj\\Desktop\\peopledata*\\index.html'):
+    with open(name, 'r', encoding='utf-8') as f:
+        bsoup = BeautifulSoup(f, 'lxml')
+    for item in bsoup.find_all('div', id='jqprint'):
+        title_div = item.find('div', class_='title')
+        title_first = title_div.h2.text
+        if title_div.h3:
+            title_second = title_div.h3.text
+        else:
+            title_second = ''
+        author_and_date = title_div.p
+        if author_and_date:
+            author = re.findall(pattern_author, author_and_date.text)[0]
+            date = re.findall(pattern_date, author_and_date.text)[0]
+        else:
+            author = ''
+            date = ''
+            if re.findall(pattern_date, title_div.text):
+                date = re.findall(pattern_date, title_div.text)[0]
+        article = item.find('div', id='detail-p').find_all('p')
+        para = ''
+        for a in article:
+            para += a.text
+
+        df = df.append({'title_first': title_first,
+                        'title_second': title_second,
+                        'author': author,
+                        'date': date,
+                        'para': para},
+                       ignore_index=True)
+
+        pass
+df = df.sort_values(by=['date'])
+df['date'] = pd.to_datetime(df['date'])
+print(df['date'])
+df.to_csv('1978to2019.csv')
--- worm/worm2.py
+++ worm/worm2.py
@@ -1,14 +1,56 @@
 import requests
-import urllib.request
+import http.cookiejar
+import urllib
+
 from bs4 import BeautifulSoup
 s = requests.session()
-url = 'http://data.people.com.cn/rmrb/20190418/1?code=2'
-url = 'https://www.baidu.com/'
-headers = {
+url_1 = 'http://data.people.com.cn/rmrb/20190418/1?code=2'
+url_2 = 'http://data.people.com.cn/sc/ss'
+headers1 = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
     AppleWebKit/537.36 (KHTML, like Gecko) \
-    Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'
+    Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134',
 }
-r = s.get(url)
-b_soup = BeautifulSoup(r)
-print(b_soup)
+headers2 = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+    AppleWebKit/537.36 (KHTML, like Gecko) \
+    Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134',
+    'Referer': 'http://data.people.com.cn/rmrb/20190418/1?code=2',
+    'Cookie': 'td_cookie=18446744073097012688; JSESSIONID=045337BC36CEEBBCD19DAD9EAD06F1C7; targetEncodinghttp://127001=2; pageNo=1; _pk_id.2.6daf=9b1a63177d9c7635.1554870540.3.1555920258.1555920223.; pageSize=50; wdcid=5bd00245f9944250'
+}
+
+data = {
+    'qs': '{"cIds":"23","cId":"23","cds":\
+    [{"fld":"dataTime.start","cdr":"AND","hlt":"false","vlr":"AND","qtp":"DEF","val":"1978-01-01"},\
+    {"fld":"dataTime.end","cdr":"AND","hlt":"false","vlr":"AND","qtp":"DEF","val":"2019-04-22"},\
+    {"cdr":"AND","cds":[{"fld":"contentText","cdr":"AND","hlt":"true","vlr":"AND","qtp":"DEF","val":"三农"}]}],\
+    "obs":[{"fld":"dataTime","drt":"DESC"}]}'
+}
+
+
+def get_cookie():
+    cookie = http.cookiejar.MozillaCookieJar('cookie.txt')
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
+    request = urllib.request.Request(url=url_1, headers=headers1)
+    opener.open(request)
+    cookie.save()
+    return cookie
+
+
+def get_follow(url,data):
+    ulist = []
+    # data = urllib.parse.urlencode(data).encode('utf-8')
+    r = requests.get(url, data=data, headers=headers2)
+    print(r.text)
+
+
+c = get_cookie()
+# cookie_dict = {i.name: i.value for i in c}
+get_follow(url_2, data)
+# print(cookie_dict)
+# headers2['Cookie'] = c
+# get_follow(url_2)
+
+
+# b_soup = BeautifulSoup(r.text, 'lxml')
+# print(b_soup)
